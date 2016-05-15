2e719600c6209b32c3a2e4ae2abde8662f65436d
Christian Fiedler <fdlr.christian@gmail.com>
2016-05-15 12:30:04 +0200

    now reading seed URLs from standard input
    this makes it possible to use more than one URL
    as starting point for farming

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ ThreadPool<CrawlWorkset,CurlProvider> *global_pool = nullptr;
 void signal_handler_exit(int sig)
 {
     LOG << "sending stop to all threads!" << std::endl;
+
     if(global_pool != nullptr)
         global_pool->send_stop();
 }
@@ -24,8 +25,7 @@ void signal_handler_exit(int sig)
 void usage() {
     using std::cerr;
     using std::endl;
-    cerr << "Usage: " << progname << " seed_domain num_threads" << endl; 
-    cerr << "seed_domain   ...   the domain, where farming is started" << endl;
+    cerr << "Usage: " << progname << " num_threads" << endl; 
     cerr << "num_threads   ...   (maximum) number of threads to use" << endl;
 }
 
@@ -34,30 +34,36 @@ int main(int argc, char **argv) {
 
     progname = argv[0];
 
-    signal(SIGINT, signal_handler_exit);
-    signal(SIGTERM, signal_handler_exit);
-
-    Log::create(std::cout);
-    LOG << "Starting..." << std::endl;
-
-    if(argc < 3){
+    if(argc != 2){
         usage();
-	return 1;
+        return 1;
     }
     
-    const char *start_url = argv[1];
-    const int num_threads = atoi(argv[2]);
+    const int num_threads = atoi(argv[1]);
 
     if(num_threads < 1 || num_threads > 5000){
         std::cerr << "num_threads must be between 1 and 5000" << std::endl;
-	return 1;
+        return 1;
     }
 
     CurlProvider provider(num_threads);
     ThreadPool<CrawlWorkset,CurlProvider> pool(num_threads, provider);
     global_pool = &pool;
 
-    pool.load( CrawlWorkset(start_url) );
+    std::cout << "Please enter some URLs. EOF or blank line will start the download. Abort with Ctrl+C." << std::endl;
+
+    std::string seed_url;
+    do{
+        getline(std::cin, seed_url);
+        pool.load( CrawlWorkset(seed_url) );
+    }while(!std::cin.eof() && !seed_url.empty());
+
+    Log::create(std::cout);
+    LOG << "Starting..." << std::endl;
+
+    signal(SIGINT, signal_handler_exit);
+    signal(SIGTERM, signal_handler_exit);
+
     pool.run(20ms);
 
     pool.join();
